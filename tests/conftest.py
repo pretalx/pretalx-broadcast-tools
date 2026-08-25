edc636f444cd26ed6156a19a73e4efe8559d03ee
Tobias Kunze <r@rixx.de>
2026-08-25 09:59:29 +0200

    Fix code style

--- tests/conftest.py
+++ tests/conftest.py
@@ -78,7 +78,9 @@ def event(organiser):
 def orga_user(event):
     with scopes_disabled():
         user = User.objects.create_user(
-            password="orgapassw0rd", email="orgauser@orga.org", name="Orga User",
+            password="orgapassw0rd",
+            email="orgauser@orga.org",
+            name="Orga User",
             email_verification_state=EmailVerificationState.VERIFIED,
         )
         team = event.organiser.teams.filter(
@@ -93,7 +95,9 @@ def orga_user(event):
 def review_user(event):
     with scopes_disabled():
         user = User.objects.create_user(
-            password="reviewpassw0rd", email="reviewuser@orga.org", name="Review User",
+            password="reviewpassw0rd",
+            email="reviewuser@orga.org",
+            name="Review User",
             email_verification_state=EmailVerificationState.VERIFIED,
         )
         team = event.organiser.teams.filter(
@@ -143,7 +147,9 @@ def submission_type(event):
 def speaker(event):
     with scopes_disabled():
         user = User.objects.create_user(
-            password="speakerpassw0rd", email="speaker@example.org", name="Jane Speaker",
+            password="speakerpassw0rd",
+            email="speaker@example.org",
+            name="Jane Speaker",
             email_verification_state=EmailVerificationState.VERIFIED,
         )
         return SpeakerProfile.objects.create(user=user, event=event)
